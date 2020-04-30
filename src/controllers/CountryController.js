5c5abe12a2a0a8b1389096efdce89fd29a999779
Artur Miguel <arturmrocha1@gmail.com>
2020-04-29 22:12:02 -0300

    Get country by name

--- src/controllers/CountryController.js
+++ src/controllers/CountryController.js
@@ -5,6 +5,18 @@ class CountryController {
         const countries = await countriesScraper()
         res.send(countries)
     }
+
+    async getCountry(req, res) {
+        const search = req.params.country
+        if (!search) return req.status(400).send({ message: 'País não informado!' })
+
+        const countries = await countriesScraper()
+        const f = countries.find(c => c.country.toLocaleLowerCase() === search.toLocaleLowerCase())
+
+        if (!f) return res.status(404).send({ message: 'País não encontrado!' })
+
+        return res.send(f)
+    }
 }
 
 module.exports = new CountryController() 
--- src/routes/CountryRouter.js
+++ src/routes/CountryRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const CountryController = require('../controllers/CountryController')
 
 router.get('/api/countries', CountryController.getCountries)
+router.get('/api/countries/:country', CountryController.getCountry)
 
 module.exports = router
